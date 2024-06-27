9949dcc8f70b40f6c92fb7d63aaeb5214cb3225c
mwierzchos <marcin.wierzchos@mechanistry.com>
2024-06-27 10:12:24 +0200

    Added HelloWorldLogger mechanistry/timberborn-issues#4806

---
+++ Assets/Mods/HelloWorld/Scripts/HelloWorldLogger.cs
@@ -0,0 +1,13 @@
+﻿using Timberborn.ModManagerScene;
+using UnityEngine;
+
+namespace Mods.HelloWorld.Scripts {
+  internal class HelloWorldLogger : IModStarter {
+
+    public void StartMod() {
+      var playerLogPath = Application.persistentDataPath + "/Player.log";
+      Debug.Log("Hello World, but in the Player.log file at: " + playerLogPath);
+    }
+
+  }
+}
