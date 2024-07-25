d34b2c57f811791cd4fef14ea2c91ee57d18cde8
mwierzchos <marcin.wierzchos@mechanistry.com>
2024-07-25 17:36:25 +0200

    Using new IModStarter method in HelloWorldLogger mechanistry/timberborn-issues#4965

--- Assets/Mods/HelloWorld/Scripts/HelloWorldLogger.cs
+++ Assets/Mods/HelloWorld/Scripts/HelloWorldLogger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace Mods.HelloWorld.Scripts {
   internal class HelloWorldLogger : IModStarter {
 
-    public void StartMod() {
+    public void StartMod(IModEnvironment modEnvironment) {
       var playerLogPath = Application.persistentDataPath + "/Player.log";
       Debug.Log("Hello World, but in the Player.log file at: " + playerLogPath);
     }
