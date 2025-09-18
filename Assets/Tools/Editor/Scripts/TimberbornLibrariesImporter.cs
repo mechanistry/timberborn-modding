f1294e45879e442f5fb6a27d33aa9c2809898e3d
Paweł Duda <pmduda86@gmail.com>
2025-09-18 13:58:42 +0200

    Extent imported types in TimberbornLibrariesImporter mechanistry/timberborn#6220

--- Assets/Tools/Editor/Scripts/TimberbornLibrariesImporter.cs
+++ Assets/Tools/Editor/Scripts/TimberbornLibrariesImporter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -14,7 +15,13 @@ namespace Timberborn.ModdingTools {
     private static readonly string PluginsDirectory = "Plugins";
     public static readonly string DllDirectory = Path.Combine(PluginsDirectory, "Timberborn");
 
+    private static readonly string ResourcesDirectory = "Resources";
+    private static readonly string ModdingDirectory = "Modding";
     private static readonly string ShadersDirectory = Path.Combine(PluginsDirectory, "Shaders");
+    private static readonly string LocalizationsDirectory =
+        Path.Combine(ResourcesDirectory, "Localizations");
+    private static readonly string UIDirectory = Path.Combine(ResourcesDirectory, "UI");
+
     private static readonly List<string> DllPatterns = new() {
         "Timberborn.*", "AWSSDK.*", "Bindito.*", "Castle.Core.dll",
         "com.rlabrecque.steamworks.net.dll", "LINQtoCSV.dll", "Moq.dll", "protobuf-net*",
@@ -23,16 +30,21 @@ namespace Timberborn.ModdingTools {
         "System.Text.Encodings.Web.dll", "System.Text.Json.dll", "Microsoft.*"
     };
     private static readonly string PublicizeDllPrefix = "Timberborn.";
-    private static readonly string ShadersZipSource = Path.Combine("Modding", "Shaders.zip");
+    private static readonly string ShadersZipSource = Path.Combine(ModdingDirectory, "Shaders.zip");
+    private static readonly string BlueprintZipSource =
+        Path.Combine(ModdingDirectory, "Blueprints.zip");
+    private static readonly string LocalizationsZipSource =
+        Path.Combine(ModdingDirectory, "Localizations.zip");
+    private static readonly string UIZipSource = Path.Combine(ModdingDirectory, "UI.zip");
 
-    [MenuItem("Timberborn/Import Timberborn DLLs and shaders", false, 0)]
+    [MenuItem("Timberborn/Import Timberborn DLLs and assets", false, 0)]
     public static void Import() {
       var timberbornLibrariesFinder = new TimberbornLibrariesFinder();
       if (timberbornLibrariesFinder.TryGetTimberbornLibrariesDirectories(
               out var dllDirectory, out var streamingAssetsDirectory)) {
         ImportDLLs(dllDirectory);
         if (streamingAssetsDirectory.Exists) {
-          ImportShaders(streamingAssetsDirectory);
+          ImportAssets(streamingAssetsDirectory);
         }
         AssetDatabase.Refresh();
       }
@@ -48,7 +60,7 @@ namespace Timberborn.ModdingTools {
           dllsCount++;
         }
       }
-      Debug.Log($"Timberborn DLLs ({dllsCount}) imported successfully.");
+      Debug.Log($"Timberborn DLLs ({dllsCount} files) imported successfully.");
     }
 
     private static void RecreateDirectory(string path) {
@@ -87,19 +99,61 @@ namespace Timberborn.ModdingTools {
       return new Guid(hash).ToString().ToLower().Replace("-", "");
     }
 
-    private static void ImportShaders(DirectoryInfo streamingAssetsDirectory) {
-      var destinationDirectory = Path.Combine(Application.dataPath, ShadersDirectory);
-      RecreateDirectory(destinationDirectory);
+    private static void ImportAssets(DirectoryInfo streamingAssetsDirectory) {
+      Import(streamingAssetsDirectory, ShadersDirectory, ShadersZipSource, "Shaders");
+      Import(streamingAssetsDirectory, ResourcesDirectory, BlueprintZipSource, "Blueprints", true);
+      Import(streamingAssetsDirectory, LocalizationsDirectory, LocalizationsZipSource,
+             "Localizations");
+      Import(streamingAssetsDirectory, UIDirectory, UIZipSource, "UI");
+    }
 
-      var shadersPath = Path.Combine(streamingAssetsDirectory.FullName, ShadersZipSource);
-      using var zipToOpen = new FileStream(shadersPath, FileMode.Open);
-      using var archive = new ZipArchive(zipToOpen, ZipArchiveMode.Read);
-      foreach (var entry in archive.Entries) {
-        var destination = Path.Combine(destinationDirectory, entry.Name);
-        entry.ExtractToFile(destination, true);
+    private static void Import(DirectoryInfo streamingAssetsDirectory, string destinationDirectory,
+                               string zipSource, string name, bool recreateSubdirectories = false) {
+      var destinationPath = Path.Combine(Application.dataPath, destinationDirectory);
+      var sourcePath = Path.Combine(streamingAssetsDirectory.FullName, zipSource);
+      if (File.Exists(sourcePath)) {
+        Import(destinationPath, sourcePath, name, recreateSubdirectories);
+      } else {
+        Debug.LogWarning($"Unable to import: {name}. Zip file not found at {sourcePath}");
       }
-      var noMetaFilesCount = archive.Entries.Count / 2;
-      Debug.Log($"Timberborn Shaders ({noMetaFilesCount}) imported successfully.");
+    }
+
+    private static void Import(string destinationPath, string sourcePath, string name,
+                               bool recreateSubdirectories) {
+      using var zipToOpen = new FileStream(sourcePath, FileMode.Open);
+      using var archive = new ZipArchive(zipToOpen, ZipArchiveMode.Read);
+      if (recreateSubdirectories) {
+        RecreateSubDirectories(destinationPath, archive);
+      } else {
+        RecreateDirectory(destinationPath);
+      }
+      foreach (var entry in archive.Entries) {
+        ImportEntry(destinationPath, entry);
+      }
+      Debug.Log($"Timberborn {name} ({archive.Entries.Count} files) imported successfully.");
+    }
+
+    private static void RecreateSubDirectories(string destinationPath, ZipArchive archive) {
+      var directories = archive.Entries
+          .Select(entry => entry.FullName.Split(new[] { '/' },
+                                                StringSplitOptions.RemoveEmptyEntries))
+          .Where(parts => parts.Length > 1)
+          .Select(parts => parts[0])
+          .Distinct();
+
+      foreach (var directory in directories) {
+        RecreateDirectory(Path.Combine(destinationPath, directory));
+      }
+    }
+
+    private static void ImportEntry(string destinationPath, ZipArchiveEntry entry) {
+      var fullNameSanitized = entry.FullName.Replace('/', Path.DirectorySeparatorChar);
+      var destination = Path.Combine(destinationPath, fullNameSanitized);
+      var entryDirectory = Path.GetDirectoryName(destination);
+      if (entryDirectory != null && !Directory.Exists(entryDirectory)) {
+        Directory.CreateDirectory(entryDirectory);
+      }
+      entry.ExtractToFile(destination, true);
     }
 
   }
