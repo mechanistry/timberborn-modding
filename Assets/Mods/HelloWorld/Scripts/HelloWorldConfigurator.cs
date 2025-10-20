732d31f1bb3717b856e6d570ce716865a96be5f6
mwierzchos <marcin.wierzchos@mechanistry.com>
2025-10-20 10:45:18 +0200

    Added IEntityPanelFragment example to HelloWorld mod mechanistry/timberborn#6347

--- Assets/Mods/HelloWorld/Scripts/HelloWorldConfigurator.cs
+++ Assets/Mods/HelloWorld/Scripts/HelloWorldConfigurator.cs
@@ -1,5 +1,6 @@
 using Bindito.Core;
 using Timberborn.BottomBarSystem;
+using Timberborn.EntityPanelSystem;
 
 namespace Mods.HelloWorld.Scripts {
   [Context("Game")]
@@ -9,8 +10,10 @@ namespace Mods.HelloWorld.Scripts {
       Bind<HelloWorldInitializer>().AsSingleton();
       Bind<HelloWorldButton>().AsSingleton();
       Bind<HelloWorldTool>().AsSingleton();
+      Bind<HelloWorldFragment>().AsSingleton();
 
       MultiBind<BottomBarModule>().ToProvider<BottomBarModuleProvider>().AsSingleton();
+      MultiBind<EntityPanelModule>().ToProvider<EntityPanelModuleProvider>().AsSingleton();
     }
 
     private class BottomBarModuleProvider : IProvider<BottomBarModule> {
@@ -24,6 +27,22 @@ namespace Mods.HelloWorld.Scripts {
       public BottomBarModule Get() {
         var builder = new BottomBarModule.Builder();
         builder.AddLeftSectionElement(_helloWorldButton, 200);
+        return builder.Build();
+      }
+
+    }
+
+    private class EntityPanelModuleProvider : IProvider<EntityPanelModule> {
+
+      private readonly HelloWorldFragment _helloWorldFragment;
+
+      public EntityPanelModuleProvider(HelloWorldFragment helloWorldFragment) {
+        _helloWorldFragment = helloWorldFragment;
+      }
+
+      public EntityPanelModule Get() {
+        var builder = new EntityPanelModule.Builder();
+        builder.AddTopFragment(_helloWorldFragment);
         return builder.Build();
       }
 
---
+++ Assets/Mods/HelloWorld/Scripts/HelloWorldFragment.cs
@@ -0,0 +1,62 @@
+﻿using Timberborn.BaseComponentSystem;
+using Timberborn.CoreUI;
+using Timberborn.EntityPanelSystem;
+using Timberborn.NaturalResources;
+using UnityEngine.UIElements;
+
+namespace Mods.HelloWorld.Scripts {
+  internal class HelloWorldFragment : IEntityPanelFragment {
+
+    private static readonly string SubPanelClass = "entity-sub-panel";
+    private static readonly string SubBoxClass = "bg-sub-box--green";
+    private static readonly string ButtonClass = "entity-fragment__button";
+    private static readonly string ButtonRedClass = "entity-fragment__button--red";
+    private static readonly string DialogBoxLocKey = "HelloWorld.DialogBoxMessage";
+
+    private readonly DialogBoxShower _dialogBoxShower;
+    private VisualElement _root;
+
+    public HelloWorldFragment(DialogBoxShower dialogBoxShower) {
+      _dialogBoxShower = dialogBoxShower;
+    }
+
+    public VisualElement InitializeFragment() {
+      _root = new NineSliceVisualElement();
+      _root.AddToClassList(SubPanelClass);
+      _root.AddToClassList(SubBoxClass);
+      _root.style.alignItems = Align.Center;
+      _root.ToggleDisplayStyle(false);
+
+      var button = new NineSliceButton {
+          text = "Hello World"
+      };
+      button.AddToClassList(ButtonClass);
+      button.AddToClassList(ButtonRedClass);
+      button.style.color = UnityEngine.Color.white;
+      button.RegisterCallback<ClickEvent>(_ => ShowDialogBox());
+      _root.Add(button);
+
+      return _root;
+    }
+
+    public void ShowFragment(BaseComponent entity) {
+      if (entity.GetComponent<NaturalResource>() is not null) {
+        _root.ToggleDisplayStyle(true);
+      }
+    }
+
+    public void ClearFragment() {
+      _root.ToggleDisplayStyle(false);
+    }
+
+    public void UpdateFragment() {
+    }
+
+    private void ShowDialogBox() {
+      _dialogBoxShower.Create()
+          .SetLocalizedMessage(DialogBoxLocKey)
+          .Show();
+    }
+
+  }
+}
