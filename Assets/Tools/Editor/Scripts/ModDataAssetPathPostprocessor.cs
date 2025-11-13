e498aba2ba13bc84805c8ec11b9f67a217df408c
mwierzchos <marcin.wierzchos@mechanistry.com>
2025-11-13 13:38:09 +0100

    Added IEditorAssetPathProcessor dedicated for Data assets mechanistry/timberborn#6472

---
+++ Assets/Tools/Editor/Scripts/ModDataAssetPathPostprocessor.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using Timberborn.AssetSystem.Editor;
+
+namespace Timberborn.ModdingTools {
+  [UsedImplicitly]
+  internal class ModDataAssetPathPostprocessor : IEditorAssetPathProcessor {
+
+    private static readonly string PrefixFolder = "/Data/";
+
+    public string ProcessForRelativePath(string path) {
+      var normalizedPath = path.Replace('\\', '/');
+      var prefixIndex = normalizedPath.IndexOf(PrefixFolder, System.StringComparison.Ordinal);
+      if (prefixIndex >= 0) {
+        return normalizedPath[(prefixIndex + PrefixFolder.Length)..];
+      }
+      return normalizedPath;
+    }
+
+    public string ProcessForLoad(string path) {
+      return path;
+    }
+
+  }
+}
